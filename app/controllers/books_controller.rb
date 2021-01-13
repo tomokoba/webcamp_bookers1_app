f6b8068f7eb1d0b747b987da7c886fbf7e113bca
tomokoba <tomokoba64@gmail.com>
2021-01-13 09:09:05 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
