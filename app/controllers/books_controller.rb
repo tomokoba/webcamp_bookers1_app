bc4a4bca109726531cc0cc5b254af1523aff09d1
tomokoba <tomokoba64@gmail.com>
2021-01-16 06:08:00 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,8 @@ class BooksController < ApplicationController
   def create
        @book = Book.new(book_params)
     if @book.save
-       redirect_to book_path(@book.id)
        flash[:success] ="Book was successfully created."
+       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render 'index'
@@ -28,10 +28,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
     flash[:success] ="Book was successfully updated."
+    redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
